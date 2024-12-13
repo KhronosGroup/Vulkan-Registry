e74a2c56c703836dc1dc7f0205c686bcde119e0a
Jon Leech <oddhack@sonic.net>
2024-12-12 17:50:01 -0800

    Trivial change to trigger webhook

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $static_title = 'Khronos Vulkan Registry';
 include_once("../../assets/static_pages/khr_page_top.php");
 ?>
 
-<p> The Vulkan registry contains formatted specifications of the Vulkan API,
+<p> Tthe Vulkan registry contains formatted specifications of the Vulkan API,
     header files, API reference pages, the reference card, and related
     documentation. The registry also links to the GitHub repository where
     the sources for these documents can be found. </p>
